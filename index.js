aefe41cdcc940db110215d39de90268e3ecadc98
sara altaee <sara.korea96@gmail.com>
2020-06-10 23:41:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,9 +27,10 @@ class Bird{
     this.sex=sex;
   }
    speak(){
-     if(this.sex==='male')
+     if(this.sex==='male'){
     return `It's me! ${this.name}, the parrot!`;
-  }
+     }
   else
   return `${this.name} says squawk!`;
+   }
 }
