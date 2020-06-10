d1ec32c26b559b752819dbd03253fde0826d714e
sara altaee <sara.korea96@gmail.com>
2020-06-10 23:34:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+class Cat{
+  constructor(name,sex){
+    
+  }
+  
+  
+}
+
+class Dog{
+  constructor(name,sex){
+    
+  }
+}
+
+class Bird{
+  constructor(name,sex){
+    
+  }
+}
